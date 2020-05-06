ffe23ef6bd05f4180af118a5277e359bfa08e3ef
Mauricio Araldi <araldi.mauricio@gmail.com>
2020-05-06 20:46:49 +0200

    Drafter working

--- src/pages/Drafter.vue
+++ src/pages/Drafter.vue
@@ -18,7 +18,8 @@
 
       <ul>
         <li v-for="hero in bans" :key="hero.id">
-          {{ hero.name }}
+          <button class="delete-button" @click="removeBan(hero)">X</button>
+          <span class="name">{{ hero.name }}</span>
         </li>
       </ul>
     </fieldset>
@@ -34,7 +35,8 @@
 
       <ul>
         <li v-for="hero in allyPicks" :key="hero.id">
-          {{ hero.name }}
+          <button class="delete-button" @click="removeAlly(hero)">X</button>
+          <span class="name">{{ hero.name }}</span>
         </li>
       </ul>
     </fieldset>
@@ -50,14 +52,15 @@
 
       <ul>
         <li v-for="hero in enemyPicks" :key="hero.id">
-          {{ hero.name }}
+          <button class="delete-button" @click="removeEnemy(hero)">X</button>
+          <span class="name">{{ hero.name }}</span>
         </li>
       </ul>
     </fieldset>
 
     <p>Hero pool</p>
     <ul>
-      <li v-for="hero in getHeroPoolSortedByScore()" :key="hero.id">
+      <li v-for="hero in heroPoolSortedByScore" :key="hero.id">
         {{ hero.name }} [{{ hero.score }}]
       </li>
     </ul>
@@ -90,6 +93,13 @@
         map: '',
       };
     },
+    computed: {
+      heroPoolSortedByScore() {
+        return Object.values(this.heroPool)
+                .map(hero => this.calculateScore(hero))
+                .sort((a, b) => b.score - a.score);
+      },
+    },
     methods: {
       allyPick() {
         if (!this.allyId) {
@@ -99,11 +109,9 @@
         const hero = this.heroes[this.allyId];
 
         this.$delete(this.heroPool, hero.id);
+        this.$set(this.allyPicks, hero.id, hero);
 
-        this.allyPicks[hero.id] = hero;
         this.allyId = '';
-
-        this.calculateHeroPoolScore();
       },
       ban() {
         if (!this.banId) {
@@ -113,38 +121,38 @@
         const hero = this.heroes[this.banId];
 
         this.$delete(this.heroPool, hero.id);
+        this.$set(this.bans, hero.id, hero);
 
-        this.bans[hero.id] = hero;
         this.banId = '';
-
-        this.calculateHeroPoolScore();
-      },
-      calculateHeroPoolScore() {
-        Object.values(this.heroPool).forEach(hero => this.calculateScore(hero));
       },
       calculateScore(hero) {
         const allies = Object.values(this.allyPicks);
         const enemies = Object.values(this.enemyPicks);
+        const { weights } = this.configurations;
 
-        hero.score = parseInt(hero.rating) * this.configurations.weights.rating;
+        hero.score = 0;
+
+        if (hero.rating) {
+          hero.score += parseInt(hero.rating) * parseInt(weights.rating);
+        }
 
         if (this.map && hero.maps[this.map]) {
-          hero.score += parseInt(hero.maps[this.map]) * this.configurations.weights.map;
+          hero.score += parseInt(hero.maps[this.map]) * parseInt(weights.map);
         }
 
         allies.forEach(ally => {
           if (hero.synergies[ally.id]) {
-            hero.score += parseInt(hero.synergies[ally.id]) * this.configurations.weights.synergy;
+            hero.score += parseInt(hero.synergies[ally.id]) * parseInt(weights.synergy);
           }
         });
 
         enemies.forEach(enemy => {
           if (hero.counters[enemy.id]) {
-            hero.score += parseInt(hero.counters[enemy.id]) * this.configurations.weights.counters;
+            hero.score += parseInt(hero.counters[enemy.id]) * parseInt(weights.counter);
           }
 
           if (enemy.counters[hero.id]) {
-            hero.score -= parseInt(enemy.counters[hero.id]) * this.configurations.weights.countered;
+            hero.score -= parseInt(enemy.counters[hero.id]) * parseInt(weights.countered);
           }
         });
 
@@ -158,22 +166,33 @@
         const hero = this.heroes[this.enemyId];
 
         this.$delete(this.heroPool, hero.id);
+        this.$set(this.enemyPicks, hero.id, hero);
 
-        this.enemyPicks[hero.id] = hero;
         this.enemyId = '';
+      },
+      removeAlly(hero) {
+        if (!hero) {
+          return;
+        }
 
-        this.calculateHeroPoolScore();
+        this.$set(this.heroPool, hero.id, hero);
+        this.$delete(this.allyPicks, hero.id);
       },
-      getFilterIds() {
-        return [
-          ...Object.keys(this.allyPicks),
-          ...Object.keys(this.enemyPicks),
-          ...Object.keys(this.bans),
-        ].map(id => parseInt(id));
+      removeBan(hero) {
+        if (!hero) {
+          return;
+        }
+
+        this.$set(this.heroPool, hero.id, hero);
+        this.$delete(this.bans, hero.id);
       },
-      getHeroPoolSortedByScore() {
-        return Object.values(this.heroPool)
-          .sort((a, b) => b.score - a.score);
+      removeEnemy(hero) {
+        if (!hero) {
+          return;
+        }
+
+        this.$set(this.heroPool, hero.id, hero);
+        this.$delete(this.enemyPicks, hero.id);
       },
     },
   };
@@ -182,5 +201,19 @@
 <style scoped>
   fieldset > label {
     display: inline-block;
+  }
+
+  ul > li:nth-child(even) {
+    background-color: #E0E0E0;
+  }
+
+  .delete-button {
+    padding: 6px 0 0;
+  }
+
+  .name {
+    display: inline-block;
+    text-align: center;
+    width: 100px;
   }
 </style>
